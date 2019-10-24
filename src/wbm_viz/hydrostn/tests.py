c406db7ce2aaeb0b63ace5fe47cd2e68359383ca
Daniel Vignoles <dvignoles@gmail.com>
2019-10-24 15:55:18 -0400

    misc renaming

--- src/wbm_viz/hydrostn/tests.py
+++ src/wbm_viz/hydrostn/tests.py
@@ -3,9 +3,10 @@ from .models import Hydrostn30Subbasin
 
 # Create your tests here.
 class SubbasinTestCase(TestCase):
+
     databases = ['argentina_01min']
 
-    def test_query_all(self):
+    def test_query_subbasin(self):
 
         #Test can query Subbasin model (maps to database view)
         self.assertEqual(Hydrostn30Subbasin.objects.count(), 48077)
