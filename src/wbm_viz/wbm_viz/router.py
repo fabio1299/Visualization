504145e287633fd1f2ec549a4e032183373a68f1
Daniel Vignoles <dvignoles@gmail.com>
2019-10-30 18:33:45 -0400

    add basic leaflet map

--- src/wbm_viz/wbm_viz/router.py
+++ src/wbm_viz/wbm_viz/router.py
@@ -4,18 +4,16 @@
 class gd_admin_db:
 
     def db_for_read(self, model, **hints):
-        # if model._meta.app_label == 'wbm_viz':
-        #     return 'default'
-        # if model._meta.app_label == 'hydrostn':
-        #     return 'argentina_01min'
-        return 'argentina_01min'
+        if model._meta.app_label == 'wbm_viz':
+            return 'default'
+        if model._meta.app_label == 'hydrostn':
+            return 'argentina_01min'
 
     def db_for_write(self, model, **hints):
-        # if model._meta.app_label == 'wbm_viz':
-        #     return 'default'
-        # if model._meta.app_label == 'hydrostn':
-        #     return 'argentina_01min'
-        return 'argentina_01min'
+        if model._meta.app_label == 'wbm_viz':
+            return 'default'
+        if model._meta.app_label == 'hydrostn':
+            return 'argentina_01min'
 
     def allow_relation(self, obj1, obj2, **hints):
         db_list = ('default', 'argentina_01min')
