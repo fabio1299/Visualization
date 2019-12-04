48998ed59c22fb7664da30b1a03b12530c809e5b
Daniel Vignoles <dvignoles@gmail.com>
2019-12-04 16:08:22 -0500

    #8 return queries as pandas dataframes

--- src/wbm_viz/wbm_viz/graphql.py
+++ src/wbm_viz/wbm_viz/graphql.py
@@ -1,6 +1,16 @@
 import requests
+import time
+import pandas as pd
 
 headers = {"Authorization": "Bearer YOUR API KEY"}
+
+MEASUREMENTS = [
+    "AirTemperature",
+    "Evapotranspiration",
+    "Precipitation",
+    "Runoff",
+    "SoilMoisture"
+]
 
 def run_query(query):
     request = requests.post('http://localhost:8080/v1/graphql', json={'query': query}, headers=headers)
@@ -9,9 +19,9 @@ def run_query(query):
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
+
 def stats_monthly(schema,parameter,unit,sample_id,year=None,month=None):
     query = """
-    {
           %s_%s_%s_monthly(where: {SampleID: {_eq: %d}, Year: {_eq: %s}, Month: {_eq: %s}},
             order_by: {Year: asc}){
                 Date
@@ -23,13 +33,41 @@ def stats_monthly(schema,parameter,unit,sample_id,year=None,month=None):
                 ZonalMin
                 ZoneArea
           }
-    }
-    
     """ % (schema, parameter,unit,sample_id, year if year else "null", month if month else "null")
     return query
 
-SAMPLE_ID =100
-YEAR = 1977
-print(run_query(stats_monthly("TerraClimate", "AirTemperature", "Subbasin", SAMPLE_ID, YEAR,)))
-print("\nNext Scehma\n")
-print(run_query(stats_monthly("WBMprist_CRUTSv401", "AirTemperature", "Subbasin", SAMPLE_ID, YEAR,)))
+
+def query_all(*args):
+    """Join one or more graphql queries into. Hasura will execute in parallel, then return together"""
+
+    all_query = "{%s}" % "".join(*args)
+    return all_query
+
+def query_model_stats_monthly(schema,unit,sample_id):
+    return query_all(
+        [stats_monthly(schema, measurement, unit, sample_id) for measurement in MEASUREMENTS]
+    )
+
+
+def query_to_df(query_result):
+    '''Returns dictionary of pandas dataframe corresponding to all querys within query_result '''
+    querys = query_result['data']
+    return {key: pd.DataFrame(val) for key, val in querys.items()}
+
+
+
+
+def main():
+    result = run_query(
+            query_model_stats_monthly("TerraClimate", "Subbasin", 1)
+        )
+
+    return result
+
+
+if __name__ == "__main__":
+    s = time.perf_counter()
+    result = main()
+    dfs = query_to_df(result)
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
