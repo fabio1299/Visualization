3e5bde25d6f8c27bff5218aa77ea55eae2977bc3
Daniel Vignoles <dvignoles@gmail.com>
2020-02-20 17:57:48 -0500

    fix typos

--- sql/aggregate_stat_tables.sql
+++ sql/aggregate_stat_tables.sql
@@ -1,30 +1,30 @@
--- DROP TABLE IF EXISTS confluence_discharge_monthly;
--- create table public.confluence_discharge_monthly
--- (
--- 	subbasin_id integer,
--- 	year integer,
--- 	month integer,
--- 	date varchar(10),
--- 	discharge double precision,
--- 	model_name text,
--- 	id serial not null
--- 		constraint confluence_discharge_monthly_pkey
--- 			primary key
--- );
---
--- create index "confluence_discharge_monthly_SampleID_idx"
--- 	on public.confluence_discharge_monthly (subbasin_id);
---
--- INSERT INTO confluence_discharge_monthly
--- SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month, "Discharge" as discharge,'Terraclimate' as model_name From "TerraClimate"."Discharge_Confluences_monthly"
--- UNION
--- SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"Discharge" as discharge,'WBMprist_CRUTSv401' as model_name From "WBMprist_CRUTSv401"."Discharge_Confluences_monthly"
--- UNION
--- SELECT "SampleID" as subbasini_d ,"Date" as date,"Year" as year,"Month" as month,"Discharge" as discharge,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."Discharge_Confluences_monthly"
--- ORDER BY
---     subbasin_id,
---     date
--- ;
+DROP TABLE IF EXISTS confluence_discharge_monthly;
+create table public.confluence_discharge_monthly
+(
+	subbasin_id integer,
+	date varchar(10),
+	year integer,
+	month integer,
+	discharge double precision,
+	model_name text,
+	id serial not null
+		constraint confluence_discharge_monthly_pkey
+			primary key
+);
+
+create index "confluence_discharge_monthly_SampleID_idx"
+	on public.confluence_discharge_monthly (subbasin_id);
+
+INSERT INTO confluence_discharge_monthly
+SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month, "Discharge" as discharge,'Terraclimate' as model_name From "TerraClimate"."Discharge_Confluences_monthly"
+UNION
+SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"Discharge" as discharge,'WBMprist_CRUTSv401' as model_name From "WBMprist_CRUTSv401"."Discharge_Confluences_monthly"
+UNION
+SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"Discharge" as discharge,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."Discharge_Confluences_monthly"
+ORDER BY
+    subbasin_id,
+    date
+;
 
 DROP TABLE IF EXISTS subbasin_air_temperature_monthly;
 create table subbasin_air_temperature_monthly
@@ -51,7 +51,7 @@ SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month
 UNION
 SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_CRUTSv401' as model_name From "WBMprist_CRUTSv401"."AirTemperature_Confluences_monthly"
 UNION
-SELECT "SampleID" as subbasini_d ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."AirTemperature_Confluences_monthly"
+SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."AirTemperature_Confluences_monthly"
 ORDER BY
     subbasin_id,
     date
@@ -82,7 +82,7 @@ SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month
 UNION
 SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_CRUTSv401' as model_name From "WBMprist_CRUTSv401"."Evapotranspiration_Confluences_monthly"
 UNION
-SELECT "SampleID" as subbasini_d ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."Evapotranspiration_Confluences_monthly"
+SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."Evapotranspiration_Confluences_monthly"
 ORDER BY
     subbasin_id,
     date
@@ -113,7 +113,7 @@ SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month
 UNION
 SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_CRUTSv401' as model_name From "WBMprist_CRUTSv401"."Precipitation_Confluences_monthly"
 UNION
-SELECT "SampleID" as subbasini_d ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."Precipitation_Confluences_monthly"
+SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."Precipitation_Confluences_monthly"
 ORDER BY
     subbasin_id,
     date
@@ -144,7 +144,7 @@ SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month
 UNION
 SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_CRUTSv401' as model_name From "WBMprist_CRUTSv401"."Runoff_Confluences_monthly"
 UNION
-SELECT "SampleID" as subbasini_d ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."Runoff_Confluences_monthly"
+SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."Runoff_Confluences_monthly"
 ORDER BY
     subbasin_id,
     date
@@ -175,7 +175,7 @@ SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month
 UNION
 SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_CRUTSv401' as model_name From "WBMprist_CRUTSv401"."SoilMoisture_Confluences_monthly"
 UNION
-SELECT "SampleID" as subbasini_d ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."SoilMoisture_Confluences_monthly"
+SELECT "SampleID" as subbasin_id ,"Date" as date,"Year" as year,"Month" as month,"ZonalMean" as zonal_mean,"ZonalMin" as zonal_min,"ZonalMax" as zonal_max,"ZoneArea" as zone_area,'WBMprist_GPCCv7' as model_name From "WBMprist_GPCCv7"."SoilMoisture_Confluences_monthly"
 ORDER BY
     subbasin_id,
     date
